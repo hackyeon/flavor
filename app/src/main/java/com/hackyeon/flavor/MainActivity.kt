7246c80b21566613d7c70cda5e976fea1a0b459f
hackyeon <skek933@gmail.com>
2022-08-30 00:54:04 +0900

    flavor commit
    flavor commit

---
+++ app/src/main/java/com/hackyeon/flavor/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.hackyeon.flavor
+
+import android.content.pm.PackageInfo
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
